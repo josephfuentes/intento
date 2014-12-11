c65db5616c73b0c83d00043b2efe218d09619273
josephfuentes <josephfuentes@unitec.edu>
2014-12-11 11:13:23 -0600

    subiendo

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include "Lista.h"
+#include "ArbolTrinario.h"
+#include "Evaluador.h"
+#include <fstream>
+#include <list>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+//2 ejercicios a realizar estan ubicados en el archivo Lista.h
+
+//Devuelve el promedio de los valres de mi_lista dada
+float obtenerPromedio(std::list<float> mi_lista)
+{
+    return -1.0f;
+}
+
+//Devuelve la suma de los valres de mi_vector dada
+int obtenerSuma(std::vector<int> mi_vector)
+{
+    return -1;
+}
+
+//Devuelve verdadero si encuentra cadena (dada) en el archivo con nombre nombre_archivo (dado), de lo contrario devuelve false
+bool existeEnArchivo(string nombre_archivo, string cadena)
+{
+    return false;
+}
+
+//Devuelve devuelve el tamano en bytes del archivo con nombre nombre_archivo (dado)
+int obtenerTamanoDeArchivo(string nombre_archivo)
+{
+    return -1;
+}
+
+//Devuelve true si encuentra el valor num (dado) en el arbol dada su raiz
+bool existeEnArbol(NodoTrinario* raiz, int num)
+{
+    return false;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
